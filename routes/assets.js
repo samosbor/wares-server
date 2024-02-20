b3916b78289b4dd9e898f73cd214ad4f7cecc014
sam <sam.osborne159@gmail.com>
2024-02-20 15:03:35 +0800

    scan front to back works

--- routes/assets.js
+++ routes/assets.js
@@ -12,42 +12,55 @@ router.post('/scan/:id', async (req, res) => {
         return
     }
     const id = req.params.id
-    const asset = getAssetByBarcode(id)
-    if (asset === null){
-        console.error('Could not find asset with this barcode/qr/rfid')
-        throw new Error('Could not find asset with this barcode/qr/rfid');
-    }
-    
+    const asset = await getAssetByBarcode(id).then((asset) => {
+        return asset
+    })
+
     const location_id = null
+    var user_id = req.body.user_id
+    if (user_id == '') {
+        await getUserByEmail(req.body.google_email).then((user) => {
+            user_id = user.user_id
+        })
+    }
 
     const insertScanEventSqlString = `INSERT INTO scan_events
   (asset_id, user_id, location_id, wifi_ap)
   VALUES($1, $2, $3, $4);`
     const { rows } = await query(insertScanEventSqlString, [
         asset.asset_id,
-        req.body.user_id,
+        user_id,
         location_id, //do some work to get this
         req.body.wifi_ap
     ])
     res.send(rows[0])
 })
 
-
 // Function to get asset by barcode
 async function getAssetByBarcode(barcode) {
     const getAssetByBarcodeSqlString = `
         SELECT * FROM assets WHERE barcode = $1
     `
-    try {
-        const { rows } = await query(getAssetByBarcodeSqlString, [barcode])
-        if (rows.length === 0) {
-            return null; // Asset not found
-        } else {
-            return rows[0]; // Return the found asset
-        }
-    } catch (error) {
-        console.error('Error fetching asset by barcode:', error)
-        throw new Error('Internal server error');
+    const { rows } = await query(getAssetByBarcodeSqlString, [barcode])
+    if (rows.length === 0) {
+        console.error('Could not find asset with this barcode/qr/rfid')
+        throw new Error('Could not find asset with this barcode/qr/rfid')
+    } else {
+        return rows[0] // Return the found asset
+    }
+}
+
+// Funtion to get user by google_email
+async function getUserByEmail(google_email) {
+    const getUserByEmailSqlString = `
+        SELECT * FROM users WHERE google_email = $1
+    `
+    const { rows } = await query(getUserByEmailSqlString, [google_email])
+    if (rows.length === 0) {
+        console.error('Could not find user by email')
+        throw new Error('Could not find user by email')
+    } else {
+        return rows[0] // Return the found user
     }
 }
 
--- server.js
+++ server.js
@@ -9,13 +9,6 @@ app.use(cors())
 app.use(express.json())
 mountRoutes(app)
 
-// log all routes available to the console
-app._router.stack.forEach((r) => {
-    if (r.route && r.route.path) {
-        console.log(r.route.path)
-    }
-})
-
 app.listen(port, () => {
     console.log(`Wares It At server listening on port ${port}`)
 })
