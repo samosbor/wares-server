149aaff7eada383a74a89b2ce49838492b05e7fc
sam <sam.osborne159@gmail.com>
2024-02-20 12:21:47 +0800

    add back promise router fml

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-import Router from 'express'
+import Router from 'express-promise-router'
 import { query } from '../db.js'
 
 const router = new Router()
 
 router.get('/:id', async (req, res) => {
+    console.log('getting user')
     const { id } = req.params
     const { rows } = await query('SELECT * FROM users WHERE user_id = $1', [id])
     res.send(rows[0])
