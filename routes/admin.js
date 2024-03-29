2ea661d5ce92f7d28c9fd0b34ea38f9c0f53888e
sam <sam.osborne159@gmail.com>
2024-03-29 14:41:55 +0800

    barcodes are booming

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,8 @@
 import Router from 'express-promise-router'
 import { query } from '../db.js'
+import bwipjs from 'bwip-js'
+import archiver from 'archiver'
+import fs from 'fs'
 
 const router = new Router()
 
@@ -7,6 +10,37 @@ router.get('/all', async (req, res) => {
     //get all the rows from the assets_extended postgres view
     const { rows } = await query('SELECT * FROM assets_extended')
     res.send(rows)
+})
+
+// post endpoint to print barcodes with bwip-js
+router.get('/print', async (req, res) => {
+    const output = fs.createWriteStream('./example.zip')
+    const archive = archiver('zip', {
+        zlib: { level: 9 } // Sets the compression level.
+    })
+
+    archive.pipe(output)
+    for (const barcode of req.query.assets.split(',')) {
+        await bwipjs
+            .toBuffer({
+                bcid: 'code128', // Barcode type
+                text: barcode, // Text to encode
+                scale: 3, // 3x scaling factor
+                height: 10, // Bar height, in millimeters
+                includetext: true, // Show human-readable text
+                textxalign: 'center' // Always good to set this
+            })
+            .then((png) => {
+                archive.append(png, { name: barcode + '.png' })
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+    }
+    archive.finalize()
+    output.on('close', () => {
+        res.download('./example.zip')
+    })
 })
 
 export default router
