4fbe87c6eabed128c0841fa0ce48220e0c31834c
sam <sam.osborne159@gmail.com>
2024-05-07 11:48:22 +0800

    chore: Add routes for admin user management

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,27 @@ router.get('/:id', async (req, res) => {
     res.send(rows[0])
 })
 
+// /admin route to get all users with admin role
+router.get('/admin', async (req, res) => {
+    const { rows } = await query('SELECT * FROM users WHERE role = $1', ['admin'])
+    res.send(rows)
+})
+
+// route to create user with fields google_user_id, google_email, role
+router.post('/', async (req, res) => {
+    const { google_user_id, google_email, role } = req.body
+    await query('INSERT INTO users (google_user_id, google_email, role) VALUES ($1, $2, $3)', [google_user_id, google_email, role])
+    res.send('User created')
+})
+
+
+// route to update user's role
+router.put('/:id', async (req, res) => {
+    const { id } = req.params
+    const { role } = req.body
+    await query('UPDATE users SET role = $1 WHERE user_id = $2', [role, id])
+    res.send('User updated')
+})
+
+
 export default router
